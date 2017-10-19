f89b794330f73909e123160ddb237bcc0dbd8eb6
Renze Nicolai <renze@rnplus.nl>
2017-10-19 17:17:31 +0200

    Added support for capabilities

--- drivers/homeyduino/device.js
+++ drivers/homeyduino/device.js
@@ -13,59 +13,75 @@ class HomeyduinoDevice extends Homey.Device {
 		//this.log("onInit() for",this.deviceName);
 		
 		this.trigger = [];
-		this.trigger.number = new Homey.FlowCardTriggerDevice("number_trigger").register();
+		this.trigger.debug = new Homey.FlowCardTriggerDevice("debug_trigger").register();
+		this.trigger.debug.registerRunListener(this.runDebugTrigger);
 		
+		this.trigger.number = new Homey.FlowCardTriggerDevice("number_trigger").register();
 		this.trigger.number.registerRunListener(this.runNumberTrigger);
+		this.trigger.number.getArgument('trigger')
+			.registerAutocompleteListener(this.onTriggerAutocomplete.bind(this));
 		
 		this.trigger.string = new Homey.FlowCardTriggerDevice("string_trigger").register();
-		
 		this.trigger.string.registerRunListener(this.runStringTrigger);
+		this.trigger.string.getArgument('trigger')
+			.registerAutocompleteListener(this.onTriggerAutocomplete.bind(this));
 		
 		this.trigger.boolean = new Homey.FlowCardTriggerDevice("boolean_trigger").register();
-		
 		this.trigger.boolean.registerRunListener(this.runBooleanTrigger);
+		this.trigger.boolean.getArgument('trigger')
+			.registerAutocompleteListener(this.onTriggerAutocomplete.bind(this));
 		
 		this.trigger.void = new Homey.FlowCardTriggerDevice("void_trigger").register();
-		
 		this.trigger.void.registerRunListener(this.runVoidTrigger);
-		
-			/*.trigger( this, {"value": this.currentOutput}, {} )
-				.then( this.log )
-				.catch( this.error );*/
+		this.trigger.void.getArgument('trigger')
+			.registerAutocompleteListener(this.onTriggerAutocomplete.bind(this));
+			
 		this.available = false;
 		
 		this._actions = [];
 		this._conditions = [];
+		this._triggers = [];
+		this._capabilities = [];
 
 		this.deviceInit();
 	}
 	
+	runDebugTrigger( args, state, callback ) {
+		return callback(null,true);
+	}
+	
 	runNumberTrigger( args, state, callback ) {
-		//console.log("runNumberTrigger");
-		if (args.trigger && state.name && args.trigger == state.name) {
+		/*console.log("runNumberTrigger");
+		console.log("ARGS",util.inspect(args, {depth: null}));
+		console.log("STATE",util.inspect(state, {depth: null}));*/
+		if (args.trigger.value && state.name && args.trigger.value == state.name) {
 			return callback(null,true);
 		}
 		return callback(null,false);
 	}
 	runStringTrigger( args, state, callback ) {
-		//console.log("runStringTrigger");
-		if (args.trigger && state.name && args.trigger == state.name) {
+		/*console.log("runStringTrigger");
+		console.log("ARGS",util.inspect(args, {depth: null}));
+		console.log("STATE",util.inspect(state, {depth: null}));*/
+		if (args.trigger.value && state.name && args.trigger.value == state.name) {
 			return callback(null,true);
 		}
 		return callback(null,false);
 	}
 	runBooleanTrigger( args, state, callback ) {
-		//console.log("runBooleanTrigger");
-		//console.log("ARGS",util.inspect(args, {depth: null}));
-		//console.log("STATE",util.inspect(state, {depth: null}));
-		if (args.trigger && state.name && args.trigger == state.name) {
+		/*console.log("runBooleanTrigger");
+		console.log("ARGS",util.inspect(args, {depth: null}));
+		console.log("STATE",util.inspect(state, {depth: null}));*/
+		if (args.trigger.value && state.name && args.trigger.value == state.name) {
 			return callback(null,true);
 		}
 		return callback(null,false);
 	}
 	runVoidTrigger( args, state, callback ) {
-		//console.log("runVoidTrigger");
-		if (args.trigger && state.name && args.trigger == state.name) {
+		/*console.log("runVoidTrigger");
+		console.log("ARGS",util.inspect(args, {depth: null}));
+		console.log("STATE",util.inspect(state, {depth: null}));*/
+		if (args.trigger.value && state.name && args.trigger.value == state.name) {
 			return callback(null,true);
 		}
 		return callback(null,false);
@@ -90,7 +106,7 @@ class HomeyduinoDevice extends Homey.Device {
 	getActions() {
 		let results = [];
 		for (var action in this._actions) {
-			results.push({"name":this._actions[action]});
+			results.push({"name":this._actions[action],"value":this._actions[action]});
 			
 		}
 		return results;
@@ -99,30 +115,66 @@ class HomeyduinoDevice extends Homey.Device {
 	getConditions() {
 		let results = [];
 		for (var condition in this._conditions) {
-			results.push({"name":this._conditions[condition]});
+			results.push({"name":this._conditions[condition],"value":this._conditions[condition]});
 			
 		}
 		return results;
 	}
 	
+	getTriggers() {
+		let results = [];
+		for (var trigger in this._triggers) {
+			results.push({"name":this._triggers[trigger],"value":this._triggers[trigger]});
+			
+		}
+		return results;
+	}
+		
 	onApiChange(info) {
 		this._actions = []; //Clear actions
 		this._conditions = []; //Clear conditions
-		this.log('Api changed.');
+		this._triggers = []; //Clear triggers
+		this._capabilities = []; //Clear capabilities
+		this.log('Info: device API changed');
 		for (var callId in info.api) {
 			let callName = info.api[callId]['name'];
 			let callType = info.api[callId]['type'];
 			if (callType=='act') {
 				this._actions.push(callName);
-				this.log('Added action',callName);
+				this.log('Info: added action',callName);
 			} else if (callType=='con') {
 				this._conditions.push(callName);
-				this.log('Added condition', callName);
+				this.log('Info: added condition', callName);
+			} else if (callType=='cap') {
+				this._capabilities.push(callName);
+				this.log('Info: added capability', callName);
+				//Note: this does NOT update the device entry.
+				//User needs to remove the device and pair again to update capabilities!
+				//It DOES update the listeners
+				this.registerCapabilityListener(callName, this.capability.bind(this, callName));
 			} else {
-				console.log('IGNORED UNKNOWN API TYPE',callType,callName);
+				this.log('Warning: ignored API',callName,'because type',callType,'is unknown.');
 			}
 		}
 	}
+	
+	getCapabilities() {
+		return this._capabilities;
+	}
+		
+	/*updateCapabilities() {
+		this._opts.capabilities = [];
+		for (var id in this._opts.api) {
+			let name = this._opts.api[id]['name'];
+			let type = this._opts.api[id]['type'];
+			if (type=='cap') {
+				this._opts.capabilities.push(name);
+				this.log('Info: added capability',name);
+			} else {
+				this.log('Warning: API',callName,'because type',callType,'is not capability.');
+			}
+		}
+	}*/
 	
 	deviceUpdateLocalAddress( callback ) {
 		callback = callback || function(){};
@@ -135,13 +187,13 @@ class HomeyduinoDevice extends Homey.Device {
 	}
 	
 	deviceInit() {
-		this.device = Homey.app.discovery.getDevice(this.deviceName);
+		this.device = Homey.app.discovery.getDevice(this.getData().id);
 		
-		//console.log(util.inspect(this.device, {depth: null}));
+		//this.log(util.inspect(this.device, {depth: null}));
 		
 		if ( this.device instanceof Error ) {
 			this.log("Homeyduino",this.deviceName,"is unavailable.");
-			this.setUnavailable("Not found");
+			this.setUnavailable("Offline");
 			this.available = false;
 		} else {
 			this.deviceUpdateLocalAddress( (err, res) => {
@@ -152,36 +204,67 @@ class HomeyduinoDevice extends Homey.Device {
 				this.device.setOpt('paired', true);
 				
 				this.device.subscribe().then( (res) => {
-					console.log('* Subscribed to triggers: ',res);
+					this.log('* Subscribed to triggers: ',res);
 				}).catch( (err) => {
-					console.log('* Could not subscribe:', err);
+					this.log('* Could not subscribe:', err);
 				});
 				
 				//Fill autocompletes
 				this.onApiChange({"device":this.device, "api":this.device.getOpt('api')});
 				
-				this.device.on('trigger', this.onTriggered.bind(this));
+				//Add listeners
+				this.device.on('emit', this.onArduinoEmit.bind(this));
 				this.device.on('api', this.onApiChange.bind(this));
 				this.device.on('master', this.onMasterChange.bind(this));
+				this.device.on('debug', this.onDeviceDebug.bind(this));
 			});
 		}
 	}
 	
+	onDeviceDebug(text) {
+		if (typeof text == "array") text = text.join(" ");
+		this.log('[njs-device]',text);
+	}
+	
+	onArduinoEmit(info) {
+		//this.log("onEmit(",info.name,info.type,info.argument,")");
+		if (info.emType=='trg') {
+			this.log("Trigger emit received:",info.name,info.type,info.argument);
+			this.onTriggered(info);
+		} else if (info.emType=='cap') {
+			this.log("Capability emit received:",info.name,info.type,info.argument);
+			this.setCapabilityValue(info.name, info.argument).catch( (err) => {
+				this.log("Could not set capability value:",err);
+			});
+		} else {
+			this.log("Unknown emit received:",info.emType,"[",info.name,info.type,info.argument,"]");
+		}
+	}
+	
 	onTriggered(info) {
-		console.log("onTriggered(",info.name,info.type,info.argument,")");
+		//console.log("INFO",util.inspect(info, {depth: null}));
+		if (!this._triggers.includes(info.name)) this._triggers.push(info.name);
 		
+		this.trigger.debug.trigger( this, {"trigger": info.name, "number": Number(info.argument), "string": String(info.argument), "boolean": Boolean(info.argument), "type":info.type}, {"name":info.name} ).then( (result) => {
+				//this.log("Debug trigger result ",result);
+			} ).catch( this.error );
 		if (info.type=='Boolean') {
-			//console.log("BOOLEAN");
-			this.trigger.boolean.trigger( this, {"value": info.argument}, {"name":info.name} ).then( /*this.log*/ ).catch( this.error );
+			this.trigger.boolean.trigger( this, {"value": info.argument}, {"name":info.name} ).then( (result) => {
+				//this.log("Boolean trigger result ",result);
+			} ).catch( this.error );
 		} else if (info.type=='Number') {
-			//console.log("NUMBER");
-			this.trigger.number.trigger( this, {"value": info.argument}, {"name":info.name} ).then( /*this.log*/ ).catch( this.error );
+			this.trigger.number.trigger( this, {"value": info.argument}, {"name":info.name} ).then( (result) => {
+				//this.log("Number trigger result ",result);
+			} ).catch( this.error );
 		} else if (info.type=='String') {
-			//console.log("STRING");
-			this.trigger.string.trigger( this, {"value": info.argument}, {"name":info.name} ).then( /*this.log*/ ).catch( this.error );
+			console.log("String trigger:",info.name, info.argument);
+			this.trigger.string.trigger( this, {"value": info.argument}, {"name":info.name} ).then( (result) => {
+				//this.log("String trigger result ",result);
+			} ).catch( this.error );
 		} else if (info.type=='null') {
-			//console.log("VOID");
-			this.trigger.void.trigger( this, {"value": info.argument}, {"name":info.name} ).then( /*this.log*/ ).catch( this.error );
+			this.trigger.void.trigger( this, {"value": info.argument}, {"name":info.name} ).then( (result) => {
+				//this.log("Void trigger result ",result);
+			} ).catch( this.error );
 		}
 	}
 	
@@ -211,18 +294,18 @@ class HomeyduinoDevice extends Homey.Device {
 						value = '0';
 					}
 				} else {
-					console.log("Non-boolean droptoken ignored!", args.droptoken, typeof args.droptoken);
+					this.log("Non-boolean droptoken ignored!", args.droptoken, typeof args.droptoken);
 				}
 			}
 			
-			return this.device.query(args.action.name, 'act', value).then( (res) => {
+			return this.device.query(args.action.value, 'act', value).then( (res) => {
 				return Promise.resolve(res);
 			}).catch( (err) => {
-				console.log('Command returned error:',err);
+				this.log('Command returned error:',err);
 				return Promise.reject(err);
 			});
 		} catch(e) {
-			console.log('Exception while executing action',e);
+			this.log('Exception while executing action',e);
 			return Promise.reject('Exception while executing action');
 		}
 	}
@@ -245,35 +328,83 @@ class HomeyduinoDevice extends Homey.Device {
 						value = '0';
 					}
 				} else {
-					console.log("Non-boolean droptoken ignored!", args.droptoken, typeof args.droptoken);
+					this.log("Non-boolean droptoken ignored!", args.droptoken, typeof args.droptoken);
 				}
 			}
 			
-			return this.device.query(args.condition.name, 'con', value).then( (res) => {
-				//console.log('Condition returned:',res);
-				//console.log('typeof result',typeof res);
+			return this.device.query(args.condition.value, 'con', value).then( (res) => {
+				//this.log('Condition returned:',res);
+				//this.log('typeof result',typeof res);
 				return Promise.resolve(res);
 			}).catch( (err) => {
-				console.log('Condition returned error:',err);
+				this.log('Condition returned error:',err);
 				return Promise.reject(err);
 			});
 		} catch(e) {
-			console.log('Exception while executing condition',e);
+			this.log('Exception while executing condition',e);
 			return Promise.reject('Exception while executing condition');
 		}
 	}
 	
+	capability(name, value, opts) {
+		this.log("Capability",name,value,opts);
+		try {
+			
+			if (typeof value === 'string') {
+				//Nothing to do
+			} else if (typeof value === 'boolean') {
+				if (value) {
+					value = '1';
+				} else {
+					value = '0';
+				}
+			} else {
+				value = value.toString();
+			}
+			
+			this.log("Processed value:",value);
+			
+			return this.device.query(name, 'cap', value).then( (res) => {
+				this.log('Capability returned:',res);
+				this.log('typeof result',typeof res);
+				return Promise.resolve(res);
+			}).catch( (err) => {
+				this.log('Capability returned error:',err);
+				return Promise.reject(err);
+			});
+		} catch(e) {
+			this.log('Exception while executing capability',e);
+			return Promise.reject('Exception while executing capability');
+		}
+	}
+	
 	onMasterChange( info ) {
-		if (this.device.getOpt('subscribed')) {
+		if ((info.master.host == this.device.getOpt('localAddress'))&&(info.master.port == this.device.getOpt('localPort'))) {
+			this.log("onMasterChange: subscribed");
+			this.device.setOpt('subscribed', true);
+		} else if (this.device.getOpt('subscribed')) {	
 			this.log("onMasterChange: re-subscribing...");
 			this.device.subscribe().then( (res) => {
-				console.log('* Subscribed to triggers: ',res);
+				this.log('* Subscribed to triggers: ',res);
 			}).catch( (err) => {
-				console.log('* Could not subscribe:', err);
+				this.log('* Could not subscribe:', err);
 			});
 		} else {
 			this.log("onMasterChange: not subscribed");
 		}
+	}
+	
+	
+	onTriggerAutocomplete(query, args) {
+		let results = args.device.getTriggers();
+		
+		results = results.filter( result => {
+			return result.name.toLowerCase().indexOf( query.toLowerCase() ) > -1;
+		});
+		
+		if (!results.includes(query)) results.push({"name":query, "value":query});
+		
+		return Promise.resolve( results );
 	}
 }
 
