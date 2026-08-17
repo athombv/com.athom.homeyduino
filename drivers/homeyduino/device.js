e42a851202c75d739379c26f95325d472f80c032
Wouter Kamp <wouter.kamp@athom.nl>
2026-08-17 14:48:31 +0200

    Fixed issue where 'All Triggers' card would not trigger
    The 'All Triggers' debug card would not trigger when type 'string' is
    used. It turns out that homey ignores flow triggers when a number
    argument is `NaN`.
    
    Updated the argument to default to 0 when no number is present.

--- drivers/homeyduino/device.js
+++ drivers/homeyduino/device.js
@@ -425,9 +425,13 @@ class HomeyduinoDevice extends Homey.Device {
         //this.log("INFO", util.inspect(info, {depth: null}));
         if (!this._triggers.includes(info.name)) this._triggers.push(info.name);
 
+        // Homey will not trigger the debug flow if `NaN` is passed as an argument, so we default to 0.
+        let debugNumber = Number(info.argument);
+        if (Number.isNaN(debugNumber)) debugNumber = 0;
+
         this.trigger.debug.trigger(this, {
             "trigger": info.name,
-            "number": Number(info.argument),
+            "number": debugNumber,
             "string": String(info.argument),
             "boolean": Boolean(info.argument),
             "type": info.type
